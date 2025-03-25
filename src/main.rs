546466c1ac4f70e19fe49cb459474fc42de8b7bc
matias-gonz <maigonzalez@fi.uba.ar>
2025-03-25 20:28:55 -0300

    feat: add basic cli app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("abstraktor")
+        .version("1.0")
+        .author("Matias Gonzalez <maigonzalez@fi.uba.ar>")
+        .about("Abstraktor")
+        .arg(Arg::new("path")
+            .long("path")
+            .required(true)
+            .help("Some path"))
+        .get_matches();
+
+    let path = matches.get_one::<String>("path").unwrap();
+    println!("{}", path);
 }
