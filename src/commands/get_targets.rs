d865ecd7d29508e3ef0a90b534265328bf7541ab
matias-gonz <maigonzalez@fi.uba.ar>
2025-04-22 19:31:05 -0300

    feat: rename instrument to get targets

--- src/commands/get_targets.rs
+++ src/commands/get_targets.rs
@@ -3,14 +3,14 @@ use clap::Parser;
 use crate::model::instrumentor::Instrumentor;
 
 #[derive(Parser, Debug)]
-pub struct InstrumentArgs {
+pub struct GetTargetsArgs {
     #[arg(short, long)]
     path: String,
 }
 
-pub fn run(args: InstrumentArgs) {
+pub fn run(args: GetTargetsArgs) {
     let content = std::fs::read_to_string(&args.path).unwrap();
     let instrumentor = Instrumentor::new();
-    let targets = instrumentor.parse_targets(&content, &args.path);
+    let targets = instrumentor.get_targets(&content, &args.path);
     println!("{:?}", targets);
 }
