d82b9163eff65600974b4f6483c92f6a9cd26685
matias-gonz <maigonzalez@fi.uba.ar>
2025-05-16 16:07:51 -0300

    feat: add get_targets function

--- src/commands/get_targets.rs
+++ src/commands/get_targets.rs
@@ -14,7 +14,7 @@ pub fn run(args: GetTargetsArgs, logger: &Logger) {
     logger.log(format!("Getting targets from {}", args.path));
     let content = std::fs::read_to_string(&args.path).unwrap();
     let instrumentor = Instrumentor::new();
-    let targets = instrumentor.get_targets(&content, &args.path);
+    let targets = instrumentor.get_targets(&[(&content, &args.path)]);
     let targets_json = serde_json::to_string_pretty(&targets).unwrap();
     std::fs::write(&args.output, targets_json).unwrap();
     logger.success(format!("Targets saved to {}", args.output));
