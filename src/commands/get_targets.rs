8f136fc3d5e953ce7fa918d2574712eab8fb4ca1
matias-gonz <maigonzalez@fi.uba.ar>
2025-05-16 16:35:16 -0300

    feat: add test_get_targets_command

--- src/commands/get_targets.rs
+++ src/commands/get_targets.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
-use std::path::Path;
 use std::fs;
+use std::path::Path;
 
 use crate::{logger::Logger, model::instrumentor::Instrumentor};
 
@@ -47,11 +47,45 @@ fn get_files_content(path: &str) -> Vec<(String, String)> {
 
 pub fn run(args: GetTargetsArgs, logger: &Logger) {
     logger.log(format!("Getting targets from {}", args.path));
-    
+
     let files = get_files_content(&args.path);
     let instrumentor = Instrumentor::new();
     let targets = instrumentor.get_targets(files);
     let targets_json = serde_json::to_string_pretty(&targets).unwrap();
     std::fs::write(&args.output, targets_json).unwrap();
     logger.success(format!("Targets saved to {}", args.output));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::logger::LogLevel;
+
+    #[test]
+    fn test_get_targets_command() {
+        let test_dir = Path::new("./tests/instrument_test");
+        let output_file = test_dir.join("targets.json");
+        let expected_file = test_dir.join("expected_targets.json");
+
+        if output_file.exists() {
+            fs::remove_file(&output_file).unwrap();
+        }
+
+        let logger = Logger::new(LogLevel::Debug);
+
+        let args = GetTargetsArgs {
+            path: test_dir.to_string_lossy().into_owned(),
+            output: output_file.to_string_lossy().into_owned(),
+        };
+        run(args, &logger);
+
+        let output = fs::read_to_string(&output_file).unwrap();
+        let expected = fs::read_to_string(&expected_file).unwrap();
+
+        let output_json: serde_json::Value = serde_json::from_str(&output).unwrap();
+        let expected_json: serde_json::Value = serde_json::from_str(&expected).unwrap();
+        assert_eq!(output_json, expected_json);
+
+        fs::remove_file(&output_file).unwrap();
+    }
 }
