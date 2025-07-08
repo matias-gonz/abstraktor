ecbd27d3f2267e63fd10bcdf128c973762f84731
Matías Ignacio González <maigonzalez@fi.uba.ar>
2025-07-08 19:18:48 -0300

    Downgrade llvm to 11 and fix vagrant deps (#56)
    * feat: add Rust and Cargo installation to Vagrantfile
    
    - Install Rust and Cargo for the vagrant user
    - Update .bashrc to include Rust environment variables
    - Set default Rust toolchain to stable
    
    * feat: add LLVM dependencies installation to Vagrantfile
    
    - Include script execution for installing LLVM dependencies in the Vagrant setup
    - Ensure proper permissions for the script execution
    
    * refactor: update LLVM version in dependencies and Makefile
    
    - Change LLVM version from 12.0.1 to 11.0.1 in deps.sh
    - Update llvm-config and clang++ references to version 11 in Makefile
    - Adjust Vagrantfile to rebuild LLVM components after dependency installation
    - Modify Rust command to use clang-11 for consistency

--- llvm/deps.sh
+++ llvm/deps.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 set -e
 
-echo "Downloading LLVM 12.0.1 archive..."
-curl -L -o clang+llvm-12.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.1/clang+llvm-12.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
+echo "Installing required dependencies..."
+sudo apt-get update
+sudo apt-get install -y libtinfo5 libncurses5
+
+echo "Downloading LLVM 11.0.1 archive..."
+curl -L -o clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
 echo "Extracting LLVM..."
-sudo tar -xf clang+llvm-12.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C /usr/local --strip-components=1
+sudo tar -xf clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C /usr/local --strip-components=1
 
-echo "Moving llvm-config to llvm-config-12"
-sudo mv /usr/local/bin/llvm-config /usr/local/bin/llvm-config-12
+echo "Moving llvm-config to llvm-config-11"
+sudo mv /usr/local/bin/llvm-config /usr/local/bin/llvm-config-11
 
-echo "Moving clang++ to clang++-12"
-sudo mv /usr/local/bin/clang++ /usr/local/bin/clang++-12
+echo "Moving clang++ to clang++-11"
+sudo mv /usr/local/bin/clang++ /usr/local/bin/clang++-11
 
 echo "Adding /usr/local/bin to PATH"
 export PATH=/usr/local/bin:$PATH
 
 echo "Checking llvm-config version:"
-llvm-config-12 --version
-clang++-12 --version
+llvm-config-11 --version
+clang++-11 --version
 
 echo "Installing nlohmann-json3-dev"
 sudo apt-get install nlohmann-json3-dev
