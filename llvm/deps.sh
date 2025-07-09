5fc2f2f90e90bf125f51e00adca0e430ee8c5636
matias-gonz <maigonzalez@fi.uba.ar>
2025-07-08 21:59:56 -0300

    chore: enhance dependency installation script for root user compatibility
    - Modify deps.sh to check for root user and adjust sudo usage accordingly
    - Ensure consistent use of sudo for all package installation and file operations

--- llvm/deps.sh
+++ llvm/deps.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 set -e
 
+# Check if we're running as root (common in containers)
+if [ "$EUID" -eq 0 ]; then
+    SUDO=""
+else
+    SUDO="sudo"
+fi
+
 echo "Installing required dependencies..."
-sudo apt-get update
-sudo apt-get install -y libtinfo5 libncurses5
+$SUDO apt-get update
+$SUDO apt-get install -y libtinfo5 libncurses5
 
 echo "Downloading LLVM 11.0.1 archive..."
 curl -L -o clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.1/clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
 
 echo "Extracting LLVM..."
-sudo tar -xf clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C /usr/local --strip-components=1
+$SUDO tar -xf clang+llvm-11.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz -C /usr/local --strip-components=1
 
 echo "Moving llvm-config to llvm-config-11"
-sudo mv /usr/local/bin/llvm-config /usr/local/bin/llvm-config-11
+$SUDO mv /usr/local/bin/llvm-config /usr/local/bin/llvm-config-11
 
 echo "Moving clang++ to clang++-11"
-sudo mv /usr/local/bin/clang++ /usr/local/bin/clang++-11
+$SUDO mv /usr/local/bin/clang++ /usr/local/bin/clang++-11
 
 echo "Adding /usr/local/bin to PATH"
 export PATH=/usr/local/bin:$PATH
@@ -25,4 +32,4 @@ llvm-config-11 --version
 clang++-11 --version
 
 echo "Installing nlohmann-json3-dev"
-sudo apt-get install nlohmann-json3-dev
+$SUDO apt-get install nlohmann-json3-dev
