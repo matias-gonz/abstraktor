e4275e7e89e0d8b1dd98e224ff692f397a7ed8ab
matias-gonz <maigonzalez@fi.uba.ar>
2025-08-19 22:09:30 -0300

    Refactor get_targets and llvm modules
    - Update get_targets.rs to suppress the result of the run function for cleaner test output.
    - Remove unused AFL_QUIET constant from llvm.rs to streamline the codebase.

--- src/commands/llvm.rs
+++ src/commands/llvm.rs
@@ -8,7 +8,6 @@ use xshell::Shell;
 const LLVM_INSTRUMENTOR_PATH: &str = "./llvm/afl-clang-fast";
 
 const AFL_CC: &str = "clang-11";
-const AFL_QUIET: &str = "1";
 
 #[derive(Parser, Debug)]
 pub struct LlvmArgs {
