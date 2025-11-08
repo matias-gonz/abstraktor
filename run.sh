e2843237566708a5ef26800ee80492bdae19e6be
user <user@user.user>
2025-11-08 19:13:14 -0300

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-cd SUT/dqlite/dqlite-raft && sudo make clean && echo "" > mipass.log && cd ../../../llvm && make && cd .. && cargo run instrument --path ./SUT/dqlite/dqlite-raft
+cd SUT/dqlite/dqlite-raft && sudo make clean && echo "" > mipass.log && cd ../../../llvm && make && cd .. && cargo run instrument --path ./SUT/dqlite/dqlite-raft/src
