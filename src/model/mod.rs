8eb8b57c0f9dac87f2588906256a875d3c6ae158
Matías Ignacio González <maigonzalez@fi.uba.ar>
2025-11-10 19:27:13 -0300

    Update mediator feedback module (#122)
    * Update mediator feedback module
    
    * Remove obsolete mediator log file from export graphs tests

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,6 +1,4 @@
 pub mod event_graph;
 pub mod instrumentor;
 
-pub use event_graph::{
-    Edge, EventGraph, NodeGraph, build_event_graph, dot_for_node, dot_for_node_graph,
-};
+pub use event_graph::{build_event_graph, dot_for_node_graph};
